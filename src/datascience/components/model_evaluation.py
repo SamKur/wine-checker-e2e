6eee753d2283d10a5159e6d43e6ed364021dcd23
Susamay Kumbhakar <susamay.sk@gmail.com>
2025-08-17 11:03:09 +0530

    newer mlflow added

--- src/datascience/components/model_evaluation.py
+++ src/datascience/components/model_evaluation.py
@@ -59,14 +59,19 @@ class ModelEvaluation:
 
 
             # Model registry does not work with file store
-            if tracking_url_type_store != "file":
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                # mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
-                mlflow.sklearn.log_model(model, "model")
+            # if tracking_url_type_store != "file":
+            #     # Register the model
+            #     # There are other ways to use the Model Registry, which depends on the use case,
+            #     # please refer to the doc for more information:
+            #     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            #     mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+            #     # mlflow.sklearn.log_model(model, "model")
 
-            else:
-                mlflow.sklearn.log_model(model, "model")
-    
+            # else:
+            #     mlflow.sklearn.log_model(model, "model")
+
+            # save model locally
+            mlflow.sklearn.save_model(model, "artifacts/model")
+
+            # log the folder as an artifact
+            mlflow.log_artifacts("artifacts/model", artifact_path="model")
